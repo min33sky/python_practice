6ac2b531e8890d8741c82416459bb96d11634509
min33sky <min33sky@naver.com>
2020-07-19 05:12:34 +0900

    hh

--- Pygame_basic_problem/tempCodeRunnerFile.py
+++ Pygame_basic_problem/tempCodeRunnerFile.py
@@ -1,3 +1,1 @@
-((1, 2, 3, 4, 5, 6, 7, 9)) == (1, 3, 7)
-    # assert easy_unpack((6, 2, 9, 4, 3, 9)) == (6, 9, 3)
-    # assert easy_unpack
+testing
--- Pygame_basic_problem/test.py
+++ Pygame_basic_problem/test.py
@@ -1,1 +1,4 @@
+str = "abdsgd"
 
+
+print(str[None:None])
