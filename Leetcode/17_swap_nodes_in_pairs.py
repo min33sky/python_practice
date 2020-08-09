9b290a731c7e02a066a88afad218ab32105a0b94
min33sky <min33sky@naver.com>
2020-08-09 18:36:23 +0900

    problem 17

--- Leetcode/17_swap_nodes_in_pairs.py
+++ Leetcode/17_swap_nodes_in_pairs.py
@@ -11,11 +11,14 @@ class ListNode:
 def swapPairs(head: ListNode) -> ListNode:
 
     if head and head.next:
-        p = head.next
-        # 스왑된 값 리턴 받음
-        head.next = swapPairs(p.next)
-        p.next = head
-        return p
+        temp = head.next
+
+        # 스왑된 값을 받는다
+        head.next = swapPairs(temp.next)
+
+        temp.next = head
+
+        return temp
 
     return head
 
@@ -23,19 +26,20 @@ def swapPairs(head: ListNode) -> ListNode:
 # 반복으로 풀기
 # def swapPairs(head: ListNode) -> ListNode:
 
-#     root = prev = ListNode(None)
+#     prev = root = ListNode(None)
 
 #     prev.next = head
 
 #     while head and head.next:
 
-#         b = head.next
-#         head.next = b.next
-#         b.next = head
+#         temp = head.next
+#         head.next = temp.next
+#         temp.next = head
 
-#         prev.next = b
+#         prev.next = temp
 
 #         head = head.next
 #         prev = prev.next.next
 
 #     return root.next
+
